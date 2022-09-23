4df4fd3bd3b7199ace7e6636a107aafad6f119d4
OMACHONU19 <113994347+OMACHONU19@users.noreply.github.com>
2022-09-23 20:49:23 +0100

    Add files via upload

---
+++ my.sol
@@ -0,0 +1,20 @@
+//Michael[] public Damilola;
+
+name-of-struct[] public name-of-array;
+
+}[11:55 AM] Fawole Damilare
+pragma solidity ^0.5.0;
+
+contract technology  {
+
+struct Michael{
+  string name;
+   uint age;
+}
+
+//Michael[] public Damilola;
+name-of-struct[] public name-of-array;
+}
+
+
+
